5444304cec2bd7a93d062ab5d4aee92cdcba8d00
Rose Mary Abraham <rosemary@keyvalue.systems>
2022-04-13 11:01:54 +0530

    Adding datetimepicker

--- src/DatePicker.ios.tsx
+++ src/DatePicker.ios.tsx
@@ -1,5 +1,6 @@
 import React, { useState } from "react";
-import { DatePickerIOS, View } from "react-native";
+import { View } from "react-native";
+import RNDateTimePicker from "@react-native-community/datetimepicker"
 
 interface Props {
   initDate: Date;
@@ -12,9 +13,9 @@ const DatePicker: React.FC<Props> = props => {
   const [date, setDate] = useState(initDate || new Date())
   return (
     <View pointerEvents={disabled ? "none" : "auto"}>
-      <DatePickerIOS
-        date={date}
-        onDateChange={(date) => {
+      <RNDateTimePicker
+        value={date}
+        onChange={(e, date) => {
           if (onDateSelected) onDateSelected(date)
           setDate(date)
         }}
